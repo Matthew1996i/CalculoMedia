885672e55b92e67c4121d0415abb42240a7c3cf0
Matthew1996i <34382193+Matthew1996i@users.noreply.github.com>
2018-07-30 20:40:37 -0300

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,9 @@
+function calc(){
+    let diasTrabalhados = document.getElementById('workedDays').value;
+    let quantidade = document.getElementById('amount').value;
+    let objetivo = document.getElementById('goal').value;
+    let diasRestantes = document.getElementById('daysRemaining').value;
+
+    console.log(diasRestantes, diasTrabalhados, quantidade, objetivo)
+}
+
