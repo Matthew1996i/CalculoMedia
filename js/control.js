a5b0a5edc166ba81a0b91f936e8afea3d57ecd45
Matthew1996i <34382193+Matthew1996i@users.noreply.github.com>
2018-08-01 00:33:40 -0300

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,9 +1,56 @@
-function calc(){
-    let diasTrabalhados = document.getElementById('workedDays').value;
-    let quantidade = document.getElementById('amount').value;
-    let objetivo = document.getElementById('goal').value;
-    let diasRestantes = document.getElementById('daysRemaining').value;
 
-    console.log(diasRestantes, diasTrabalhados, quantidade, objetivo)
+
+function calc(e){
+
+    let diasTrabalhados = document.getElementById('workedDays').value
+    let quantidade = document.getElementById('amount').value
+    let objetivo = document.getElementById('goal').value
+    let diasRestantes = document.getElementById('daysRemaining').value
+    let mediaProdutividade = quantidade/diasTrabalhados
+    let projecao = mediaProdutividade*diasRestantes
+    let percentualProjecao = projecao/objetivo
+
+    
+   
+    const calcTrabalho = ()=>{
+        if(diasTrabalhados && quantidade >= 1){
+            document.getElementById('displayMedia').value = `${Math.floor(mediaProdutividade)} Acionamentos`
+            document.getElementById('displayMeta').value = ` ${Math.floor(projecao)} em ${diasRestantes} Dias/ ${percentualProjecao.toFixed(2)}% da Meta` 
+        }else{
+            document.getElementById('displayMedia').value = `Valores Inválidos`
+            alert(`Preencha Dias Trabalhados e Acionamentos com Valores Validos`)
+        }
+    }
+
+    calcTrabalho()
+
+    const calcValor = ()=>{
+        if(mediaProdutividade >= 40 && mediaProdutividade <= 44) {
+            document.getElementById('faixaValor').value = `R$ 40,00`
+
+        }else if(mediaProdutividade >= 45 && mediaProdutividade <=49){
+            document.getElementById('faixaValor').value = `R$ 80,00`
+
+        }else if(mediaProdutividade >= 50 && mediaProdutividade <= 70){
+            document.getElementById('faixaValor').value = `R$ 120,00`
+
+        }else{
+            document.getElementById('faixaValor').value = `R$ 00,00`
+        }
+    }
+    calcValor()
+
+    
+    
 }
 
+
+function reset(){
+    document.getElementById('displayMedia').value = ''
+    document.getElementById('displayMeta').value = ''
+    document.getElementById('faixaValor').value = ''
+    document.getElementById('workedDays').value = ''
+    document.getElementById('amount').value = ''
+    document.getElementById('goal').value = ''
+    document.getElementById('daysRemaining').value = ''
+}
